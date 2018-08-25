fc85d605c496a50056143c48965b21cef028483d
Filippos Vasilakis <vasilakisfil@gmail.com>
2018-08-25 22:24:34 +0200

    Improve AMS adapter

--- lib/simple_ams/adapters/ams.rb
+++ lib/simple_ams/adapters/ams.rb
@@ -10,18 +10,44 @@ class SimpleAMS::Adapters::AMS
   def as_json
     hash = {}
 
-    document.fields.each{ |field|
-      hash[field.key] = field.value.as_json
-    }
-
-    document.relations.each{|relation|
-      hash[relation.name] = relation.as_json
-    }
+    #TODO: I think bang method for merging is way faster ?
+    hash = hash.merge(fields)
+    hash = hash.merge(relations)
+    hash = hash.merge(links: links) unless links.empty?
+    hash = hash.merge(metas: metas) unless metas.empty?
 
     return hash
   end
 
-  class Collection
+  def fields
+    @fields ||= document.fields.inject({}){ |hash, field|
+      hash[field.key] = field.value.as_json
+      hash
+    }
+  end
+
+  def relations
+    @relations ||= document.relations.inject({}){ |hash, relation|
+      hash[relation.name] = relation.as_json
+      hash
+    }
+  end
+
+  def links
+    @links ||= document.links.inject({}){ |hash, link|
+      hash[link.name] = link.value.as_json
+      hash
+    }
+  end
+
+  def metas
+    @metas ||= document.metas.inject({}){ |hash, meta|
+      hash[meta.name] = meta.value.as_json
+      hash
+    }
+  end
+
+  class Collection < self
     attr_reader :folder, :adapter
 
     def initialize(folder)
@@ -38,6 +64,5 @@ class SimpleAMS::Adapters::AMS
         adapter.new(document).as_json
       } || []
     end
-
   end
 end
