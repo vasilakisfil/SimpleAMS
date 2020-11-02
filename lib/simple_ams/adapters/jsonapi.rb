d665ca80c8752db9e544dd3ffad419820e5b06eb
Danilo de Lima <danilolicd@gmail.com>
2020-11-02 14:38:51 -0300

    Rubocop in Action to adapters

--- lib/simple_ams/adapters/jsonapi.rb
+++ lib/simple_ams/adapters/jsonapi.rb
@@ -1,10 +1,10 @@
-require "simple_ams"
+require 'simple_ams'
 
 class SimpleAMS::Adapters::JSONAPI
   DEFAULT_OPTIONS = {
     skip_id_in_attributes: false,
     key_transform: nil
-  }
+  }.freeze
 
   attr_reader :document, :options
   def initialize(document, options = {})
@@ -14,21 +14,21 @@ class SimpleAMS::Adapters::JSONAPI
 
   def as_json
     hash = {
-      data: data,
+      data: data
     }
 
     hash.merge!(links: links) unless links.empty?
     hash.merge!(metas: metas) unless metas.empty?
     hash.merge!(included: included) unless included.empty?
 
-    return hash
+    hash
   end
 
   def data
     data = {
       transform_key(document.primary_id.name) => document.primary_id.value.to_s,
       type: document.type.value,
-      attributes: fields,
+      attributes: fields
     }
 
     data.merge!(relationships: relationships) unless relationships.empty?
@@ -37,12 +37,9 @@ class SimpleAMS::Adapters::JSONAPI
   end
 
   def fields
-    @fields ||= document.fields.inject({}){ |hash, field|
-      unless options[:skip_id_in_attributes] && field_is_primary_id?(field)
-        hash[transform_key(field.key)] = field.value
-      end
-      hash
-    }
+    @fields ||= document.fields.each_with_object({}) do |field, hash|
+      hash[transform_key(field.key)] = field.value unless options[:skip_id_in_attributes] && field_is_primary_id?(field)
+    end
   end
 
   def transform_key(key)
@@ -50,7 +47,7 @@ class SimpleAMS::Adapters::JSONAPI
     when :camel
       key.to_s.split('_').map(&capitalize).join
     when :kebab
-      key.to_s.gsub('_','-')
+      key.to_s.gsub('_', '-')
     when :snake
       key
     else
@@ -61,36 +58,29 @@ class SimpleAMS::Adapters::JSONAPI
   def relationships
     return @relationships ||= {} if document.relations.empty?
 
-    @relationships ||= document.relations.inject({}){ |hash, relation|
-      _hash = {}
-
+    @relationships ||= document.relations.each_with_object({}) do |relation, hash|
       embedded_relation_data = embedded_relation_data_for(relation)
-      unless embedded_relation_data.empty?
-        _hash.merge!(data: embedded_relation_data_for(relation))
-      end
+      hash.merge!(data: embedded_relation_data_for(relation)) unless embedded_relation_data.empty?
 
       embedded_relation_links = embedded_relation_links_for(relation)
-      unless embedded_relation_links.empty?
-        _hash.merge!(links: embedded_relation_links_for(relation))
-      end
+      hash.merge!(links: embedded_relation_links_for(relation)) unless embedded_relation_links.empty?
 
-      hash.merge!(relation.name => _hash) unless _hash.empty?
-      hash
-    }
+      hash.merge!(relation.name => hash) unless hash.empty?
+    end
   end
 
   def embedded_relation_data_for(relation)
     return {} if relation.embedded.generics[:skip_data]&.value
 
     if relation.folder?
-      value = relation.each.map{|doc|
+      relation.each.map do |doc|
         {
           doc.primary_id.name => doc.primary_id.value.to_s,
           type: doc.type.name
         }
-      }
+      end
     else
-      value = {
+      {
         relation.primary_id.name => relation.primary_id.value.to_s,
         type: relation.type.name
       }
@@ -100,50 +90,43 @@ class SimpleAMS::Adapters::JSONAPI
   def embedded_relation_links_for(relation)
     return {} if relation.embedded.links.empty?
 
-    relation.embedded.links.inject({}){ |hash, link|
+    relation.embedded.links.each_with_object({}) do |link, hash|
       hash[link.name] = link.value
-      hash
-    }
+    end
   end
-
 
   def links
     return @links ||= {} if document.links.empty?
 
-    @links ||= document.links.inject({}){ |hash, link|
+    @links ||= document.links.each_with_object({}) do |link, hash|
       hash[link.name] = link.value
-      hash
-    }
+    end
   end
 
   def metas
-    @metas ||= document.metas.inject({}){ |hash, meta|
+    @metas ||= document.metas.each_with_object({}) do |meta, hash|
       hash[meta.name] = meta.value
-      hash
-    }
+    end
   end
 
   def forms
-    @forms ||= document.forms.inject({}){ |hash, form|
+    @forms ||= document.forms.each_with_object({}) do |form, hash|
       hash[form.name] = form.value
-      hash
-    }
+    end
   end
 
   def included
     return @included ||= [] if document.relations.available.empty?
 
-    @included ||= document.relations.available.inject([]){ |array, relation|
+    @included ||= document.relations.available.each_with_object([]) do |relation, array|
       next array if relation.embedded.generics[:skip_data]&.value
 
-      if relation.folder?
-        array << relation.map{|doc| self.class.new(doc, options).as_json[:data]}
-      else
-        array << self.class.new(relation, options).as_json[:data]
-      end
-
-      array
-    }.flatten
+      array << if relation.folder?
+                 relation.map { |doc| self.class.new(doc, options).as_json[:data] }
+               else
+                 self.class.new(relation, options).as_json[:data]
+               end
+    end.flatten
   end
 
   class Collection < self
@@ -163,16 +146,16 @@ class SimpleAMS::Adapters::JSONAPI
       hash.merge!(links: links) unless links.empty?
       hash.merge!(included: included) unless included.empty?
 
-      return hash
+      hash
     end
 
     def documents
       @included = []
-      return folder.map{|document|
+      folder.map  do |document|
         _doc = adapter.new(document, options).as_json
         @included << _doc[:included]
         _doc[:data]
-      } || []
+      end || []
     end
 
     def included
@@ -180,24 +163,21 @@ class SimpleAMS::Adapters::JSONAPI
     end
 
     def metas
-      @metas ||= folder.metas.inject({}){ |hash, meta|
+      @metas ||= folder.metas.each_with_object({}) do |meta, hash|
         hash[transform_key(meta.name)] = meta.value
-        hash
-      }
+      end
     end
 
     def links
-      @links ||= folder.links.inject({}){ |hash, link|
+      @links ||= folder.links.each_with_object({}) do |link, hash|
         hash[transform_key(link.name)] = link.value
-        hash
-      }
+      end
     end
   end
 
   private
-    def field_is_primary_id?(field)
-      field.key == document.primary_id.name
-    end
 
+  def field_is_primary_id?(field)
+    field.key == document.primary_id.name
+  end
 end
-
